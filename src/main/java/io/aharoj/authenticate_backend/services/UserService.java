6a24db9a0e3330dbb7b9baf563ad8743155b1fb5
Angel J. Haro <haro.j.angel@gmail.com>
2024-11-14 22:02:56 -0800

    service: user

---
+++ src/main/java/io/aharoj/authenticate_backend/services/UserService.java
@@ -0,0 +1,51 @@
+package io.aharoj.authenticate_backend.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import io.aharoj.authenticate_backend.models.ApplicationUser;
+import io.aharoj.authenticate_backend.models.Role;
+import io.aharoj.authenticate_backend.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+  @Autowired
+  private PasswordEncoder encoder;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    System.out.println("In the user details service");
+
+    return userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("user is not valid"));
+  }
+  /**
+   * @Override
+   *           public UserDetails loadUserByUsername(String username) throws
+   *           UsernameNotFoundException {
+   * 
+   *           System.out.println("In the user details service");
+   * 
+   *           if (!username.equals("Angel"))
+   *           throw new UsernameNotFoundException("Not Angel");
+   * 
+   *           Set<Role> roles = new HashSet<>();
+   *           roles.add(new Role(1, "USER"));
+   * 
+   *           return new ApplicationUser(1, "Angel", encoder.encode("password"),
+   *           roles);
+   *           }
+   */
+}
