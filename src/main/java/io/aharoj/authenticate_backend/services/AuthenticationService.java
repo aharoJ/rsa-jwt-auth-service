352dc86168038e25a2a2f2c7e48802f386d28483
Angel J. Haro <haro.j.angel@gmail.com>
2024-11-19 12:40:20 -0800

    service: auth + token

--- src/main/java/io/aharoj/authenticate_backend/services/AuthenticationService.java
+++ src/main/java/io/aharoj/authenticate_backend/services/AuthenticationService.java
@@ -4,11 +4,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import io.aharoj.authenticate_backend.models.ApplicationUser;
+import io.aharoj.authenticate_backend.models.LoginResponseDTO;
 import io.aharoj.authenticate_backend.models.Role;
 import io.aharoj.authenticate_backend.repository.RoleRepository;
 import io.aharoj.authenticate_backend.repository.UserRepository;
@@ -26,6 +31,12 @@ public class AuthenticationService {
   @Autowired
   private PasswordEncoder passwordEncoder;
 
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private TokenService tokenService;
+
   public ApplicationUser registerUser(String username, String password) {
 
     String encodedPassword = passwordEncoder.encode(password);
@@ -34,7 +45,23 @@ public class AuthenticationService {
     Set<Role> authorities = new HashSet<>();
 
     authorities.add(userRole);
+
     return userRepository.save(new ApplicationUser(0, username, encodedPassword, authorities));
+  }
+
+  public LoginResponseDTO loginUser(String username, String password) {
+
+    try {
+      Authentication auth = authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(username, password));
+
+      String token = tokenService.generateJwt(auth);
+
+      return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
+
+    } catch (AuthenticationException e) {
+      return new LoginResponseDTO(null, "");
+    }
   }
 
 }
